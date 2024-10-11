167770d34a1e79a6d6baf56edab3292fbb4be99e
asaken1021 <asaken1021@outlook.com>
2024-10-11 17:45:48 +0900

    add VMVideo

---
+++ app/src/main/java/net/asaken1021/vmmanager/util/VMVideo.java
@@ -0,0 +1,17 @@
+package net.asaken1021.vmmanager.util;
+
+public class VMVideo {
+    private String type;
+
+    public VMVideo(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
