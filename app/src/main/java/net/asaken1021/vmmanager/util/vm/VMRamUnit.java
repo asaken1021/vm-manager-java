7328c81a7fb501abeebd0f702f9bcf0f8f9e30c3
asaken1021 <asaken@asaken1021.net>
2024-10-29 17:44:48 +0900

    add unitText

--- app/src/main/java/net/asaken1021/vmmanager/util/vm/VMRamUnit.java
+++ app/src/main/java/net/asaken1021/vmmanager/util/vm/VMRamUnit.java
@@ -1,6 +1,28 @@
 package net.asaken1021.vmmanager.util.vm;
 
+import net.asaken1021.vmmanager.util.TypeNotFoundException;
+
 public enum VMRamUnit {
-    RAM_GiB, 
-    RAM_MiB
+    RAM_GiB("GiB"), 
+    RAM_MiB("MiB");
+
+    private String unitText;
+
+    private VMRamUnit(String unitText) {
+        this.unitText = unitText;
+    }
+
+    public String getUnitText() {
+        return this.unitText;
+    }
+
+    public static VMRamUnit getUnitByString(String unit) throws TypeNotFoundException {
+        if (unit.equals(VMRamUnit.RAM_GiB.getUnitText())) {
+            return VMRamUnit.RAM_GiB;
+        } else if (unit.equals(VMRamUnit.RAM_MiB.getUnitText())) {
+            return VMRamUnit.RAM_MiB;
+        }
+
+        throw new TypeNotFoundException();
+    }
 }
